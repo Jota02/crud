604a5a210ef906df8e4000ac041aa8709ded5d20
Pin7i7o <118998982+Pin7i7o@users.noreply.github.com>
2023-12-21 16:39:48 +0000

    sign-in/up feito

--- pages/public/signin.php
+++ pages/public/signin.php
@@ -4,38 +4,47 @@ include_once __DIR__ . '../../../templates/header.php';
 
 $title = ' - Sign In';
 ?>
-<main class = "text-center">
-  <section>
-    <?php
-    if (isset($_SESSION['errors'])) {
-      echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">';
-      foreach ($_SESSION['errors'] as $error) {
-        echo $error . '<br>';
-      }
-      echo '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
-      unset($_SESSION['errors']);
-    }
-    ?>
-  </section>
-  <form action="/crud/controllers/auth/signin.php" method="post">
-    <h1 class="h3 mb-3 fw-normal">Sign In</h1>
-    <div class="form-floating mb-2">
-      <input type="email" class="form-control" id="Email" placeholder="Email" name="email" maxlength="255"
-        value="<?= isset($_REQUEST['email']) ? $_REQUEST['email'] : null ?>">
-      <label for="Email">Email</label>
+
+  <div class="d-flex justify-content-center align-items-center" style="min-height: 100vh;">
+    <div class="container py-5">  
+        <div class="account-form-wrapper text-white">
+          <h1 class="mb-3 fw-normal text-center">Sign In</h1>
+          <form action="/crud/controllers/auth/signin.php" method="post">
+            <div class="account-form-style">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="Email" maxlength="255" name="email"
+                  value="<?= isset($_REQUEST['email']) ? $_REQUEST['email'] : null ?>">
+              </div>
+              <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" maxlength="255" name="password"
+                  value="<?= isset($_REQUEST['password']) ? $_REQUEST['password'] : null ?>">
+              </div>
+              <div class="checkbox mb-3">
+                <label><input type="checkbox" value="remember-me"> Remember me</label>
+              </div>
+              <button class="w-100 btn btn-lg btn-outline-dark btn-success mb-2 text-white" type="submit" name="user" value="login">Sign In</button>
+            </div>
+          </form>
+          <a href="/crud">
+                <button class="w-100 btn btn-lg btn-outline-dark btn-danger mb-2 text-white">Back</button>
+              </a>
+          <section>
+            <?php
+              if (isset($_SESSION['errors'])) {
+                echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">';
+                foreach ($_SESSION['errors'] as $error) {
+                  echo $error . '<br>';
+                }
+                echo '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
+                unset($_SESSION['errors']);
+              }
+            ?>
+          </section>
+        </div>
     </div>
-    <div class="form-floating mb-2">
-      <input type="password" class="form-control" id="password" placeholder="Password" name="password" maxlength="255"
-        value="<?= isset($_REQUEST['password']) ? $_REQUEST['password'] : null ?>">
-      <label for="password">Password</label>
-    </div>
-    <div class="checkbox mb-3">
-      <label><input type="checkbox" value="remember-me">Remember me</label>
-    </div>
-    <button class="w-100 btn btn-lg btn-success mb-2" type="submit" name="user" value="login">Sign In</button>
-  </form>
-  <a href="/crud"><button class="w-100 btn btn-lg btn-info">Back</button></a>
-</main>
+  </div>
 
 <div class= "fixed-bottom">
     <?php
--- pages/public/signup.php
+++ pages/public/signup.php
@@ -3,50 +3,58 @@ require_once __DIR__ . '/../../infra/middlewares/middleware-not-authenticated.ph
 $title = '- Sign Up';
 include_once __DIR__ . '../../../templates/header.php'; ?>
 
-<main class = "text-center">
-  <section>
-    <?php
-    if (isset($_SESSION['success'])) {
-      echo '<div class="alert alert-success alert-dismissible fade show" role="alert">';
-      echo $_SESSION['success'] . '<br>';
-      echo '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
-      unset($_SESSION['success']);
-    }
-    if (isset($_SESSION['errors'])) {
-      echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">';
-      foreach ($_SESSION['errors'] as $error) {
-        echo $error . '<br>';
-      }
-      echo '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
-      unset($_SESSION['errors']);
-    }
-    ?>
-  </section>
-  <form action="/crud/controllers/auth/signup.php" method="post">
-    <h1 class="h3 mb-3 fw-normal">Sign Up</h1>
-    <div class="form-floating mb-2">
-      <input type="text" class="form-control" name="name" placeholder="name" maxlength="100" size="100"
-        value="<?= isset($_REQUEST['name']) ? $_REQUEST['name'] : null ?>" required>
-      <label for="name">Name</label>
+<div class="d-flex justify-content-center align-items-center" style="min-height: 100vh;">
+    <div class="container py-5">  
+        <div class="account-form-wrapper text-white">
+          <h1 class="mb-3 fw-normal text-center">Sign Up</h1>
+          <form action="/crud/controllers/auth/signup.php" method="post">
+            <div class="account-form-style">
+              <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control" id="name" maxlength="255" name="name"
+                  value="<?= isset($_REQUEST['name']) ? $_REQUEST['name'] : null ?>">
+              </div>
+              <div class="mb-3">
+                <label for="email" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="Email" maxlength="255" name="email"
+                  value="<?= isset($_REQUEST['email']) ? $_REQUEST['email'] : null ?>">
+              </div>
+              <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" maxlength="255" name="password"
+                  value="<?= isset($_REQUEST['password']) ? $_REQUEST['password'] : null ?>">
+              </div>
+              <div class="mb-3">
+                <label for="password-confirmation" class="form-label">Confirm Password</label>
+                <input type="password" class="form-control" id="password-confirmation" maxlength="255" name="password-confirmation">
+              </div>
+              <button class="w-100 btn btn-lg btn-outline-dark btn-success mb-2 text-white" type="submit" name="user" value="signUp">Sign Up</button>
+            </div>
+          </form>
+          <a href="/crud">
+                <button class="w-100 btn btn-lg btn-outline-dark btn-danger mb-2 text-white">Back</button>
+          </a>
+          <section>
+            <?php
+            if (isset($_SESSION['success'])) {
+              echo '<div class="alert alert-success alert-dismissible fade show" role="alert">';
+              echo $_SESSION['success'] . '<br>';
+              echo '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
+              unset($_SESSION['success']);
+            }
+            if (isset($_SESSION['errors'])) {
+              echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">';
+              foreach ($_SESSION['errors'] as $error) {
+                echo $error . '<br>';
+              }
+              echo '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
+              unset($_SESSION['errors']);
+            }
+            ?>
+          </section>
+        </div>
     </div>
-    <div class="form-floating mb-2">
-      <input type="email" class="form-control" id="floatingInput" name="email" placeholder="name@example.com"
-        value="<?= isset($_REQUEST['email']) ? $_REQUEST['email'] : null ?>">
-      <label for="floatingInput">Email</label>
-    </div>
-    <div class="form-floating mb-2">
-      <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-      <label for="password">Password</label>
-    </div>
-    <div class="form-floating mb-2">
-      <input type="password" class="form-control" id="confirmar_palavra_passe" name="confirmar_palavra_passe"
-        placeholder="Confirm password">
-      <label for="confirmar_palavra_passe">Confirm Password</label>
-    </div>
-    <button class="w-100 btn btn-lg btn-success mb-2" type="submit" name="user" value="signUp">Sign Up</button>
-  </form>
-  <a href="/crud/"><button class="w-100 btn btn-lg btn-info">Back</button></a>
-</main>
+  </div>
 
 <div class= "fixed-bottom">
     <?php
