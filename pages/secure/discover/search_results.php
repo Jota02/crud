2a35ff6c5fbdfb9b28f34d276cdeb7374609ed0b
Pin7i7o <118998982+Pin7i7o@users.noreply.github.com>
2024-01-11 19:28:05 +0000

    Adicionada search bar aos search_results

--- pages/secure/discover/search_results.php
+++ pages/secure/discover/search_results.php
@@ -17,6 +17,16 @@ include_once __DIR__ . '/../../../templates/navbar.php';
 <!-- Content -->
 <div class="container" style="min-height: 100vh">
     <div class="d-flex flex-column main-margin">
+        <div class="d-flex py-5 justify-content-center search-bar w-100">
+            <form action="/crud/controllers/shows/shows.php" method="get" class="d-flex w-100 justify-content-center">
+                <div class="input-group search-bar-size w-100">
+                    <input type="text" name="searchInput" class="form-control" placeholder="Search for shows..." maxlength="255">
+                    <button type="submit" name="submitSearch" class="btn btn-default">
+                        <i class="bi bi-search search-icon"></i>
+                    </button>
+                </div>
+            </form>
+        </div>
         <div class="d-flex mb-5 align-items-center">
             <i class="bi bi-search search-icon me-3"></i>
             <h1 class="text-white m-0">Search -> "<?= $searchInput ?>"</h1>    
@@ -24,7 +34,7 @@ include_once __DIR__ . '/../../../templates/navbar.php';
         <div class="heading">
             <h3 class=" ms-3 text-white text-center">Shows</h3>
         </div>
-        <div class="show-list mt-4">
+        <div class="show-list mt-3 mb-5">
             <div class="list-group">
                 <?php foreach ($searchResults as $result): 
                         $show = getShowById($result['id'])    
